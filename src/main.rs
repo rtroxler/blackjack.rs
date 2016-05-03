4a0e9f061e956bc534068625df7594d29db05b78
Ryan Troxler <rtroxler@rednovalabs.com>
2016-05-03 08:52:37 -0500

    Hit and pull cards

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,34 @@ pub mod deck;
 use std::io;
 use std::str;
 use deck::Deck;
+use deck::Card;
+
+struct Hand {
+    cards: Vec<Card>
+}
+
+impl Hand {
+    //fn value(&self) -> i32 {
+        ////println!("{:?}", self.cards)
+    //}
+}
 
 fn main() {
-    println!("#########################");
-    println!("# Welcome to Blackjack! #");
-    println!("#########################");
-    // need to have a hand, composed of two cards
+    println!("#########################################");
+    println!("######### Welcome to Blackjack! #########");
+    println!("#########################################");
 
     // shuffle a new deck
     let mut deck = Deck::new();
 
-    // will wanna mut
-    let hand = deck.deal(2);
-    println!("\nHere's your hand: {:?}", hand);
+    // will wanna mut so we can hit
+    let mut player_hand = deck.deal(2);
+    println!("\n   You: {:?}", player_hand);
 
+    let computer_hand = deck.deal(2);
+    println!("Dealer: [{:?}, X]", computer_hand[0]);
+
+    let mut bust = false;
     loop {
         println!("Would you like to hit or stand?");
 
@@ -27,9 +41,10 @@ fn main() {
             .expect("Failed to read line");
 
         if response.trim() == "hit" {
-            println!(" Finna hit");
+            player_hand.extend(deck.deal(1));
+            println!("Your hand is now: {:?}", player_hand);
         } else if response.trim() == "stand" {
-            println!(" Finna stand");
+            break;
         } else {
             println!("I could not understand your response. Try again.");
         }
